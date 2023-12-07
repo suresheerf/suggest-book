145c49a1d8f26b6d232ee6e43f40c5adc63662b1
suresh d <suresheerf@gmail.com>
2023-12-07 10:36:25 +0530

    initial student schema

--- app/student/model.js
+++ app/student/model.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const studentSchema = new mongoose.Schema(
   {
-    
+    ageGroup:{type:String,enum:['child','teen','youngAdult','adult','middleAged','senior']},
+    gender: {type:String,enum:['male','female','other']}
   },
   { timestamps: true }
 );
